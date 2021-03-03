c28060daee132e92096f752ce4b4af176371e1fa
cleverlee <dlchdaud1328@gmail.com>
2021-03-04 01:44:45 +0900

    c1-commit

--- hospital_reservation(test-2)/app.py
+++ hospital_reservation(test-2)/app.py
@@ -6,8 +6,9 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
 SECRET_KEY = 'SPARTA'
-from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
@@ -15,15 +16,28 @@ db = client.dbsparta
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+        user_info = db.users.find_one({"id": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
 
 @app.route('/reg')
 def register():
     return render_template('register.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+
 
 
 ###########################################################
@@ -120,19 +134,19 @@ def sign_up():
 
 ###################################################################################
 ###############################힘찬님 코드###################################
-@app.route('/sign_in', methods=['POST'])
+@app.route('/sign_in2', methods=['POST'])
 def sign_in():
     # 로그인
     username_receive = request.form['user_id_give']
     password_receive = request.form['user_pw_give']
 
-    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'id': username_receive, 'pw': pw_hash})
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'id': username_receive, 'pw': password_hash})
 
     if result is not None:
         payload = {
             'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'exp': datetime.utcnow() + timedelta(seconds=5)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
